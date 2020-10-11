d5cd42e1d781077c9a9547264c6691346867069c
po1nt-1 <po1nt@protonmail.ch>
2020-10-11 22:52:40 +1000

    added Transfer to synchronize variables between classes. WorkerThread moved to db.

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import multiprocessing as mp
 import os
 import sys
+import time
 
-from PySide2 import QtCore, QtGui
+from PySide2 import QtCore
 from PySide2.QtWidgets import (QApplication, QFileDialog, QMainWindow,
                                QMessageBox, QTableWidgetItem)
 
 import db
 import front
-import time
-
-
-g_path_to_csv = ""
 
 
 class MyQtApp(front.Ui_MainWindow, QMainWindow):
     def __init__(self, parent=None):
-        super(MyQtApp, self).__init__()
+        super(MyQtApp, self).__init__(parent)
         self.setupUi(self)
-        self.button_import.clicked.connect(
-            self.import_to_db)
-        self.button_forward.clicked.connect(self.forward)
-        self.button_back.clicked.connect(self.back)
+
+        self.button_import.clicked.connect(self.open_file_browser)
 
     def open_file_browser(self):
-        global g_path_to_csv
-
         print("open_file_browser")
 
         g_path_to_csv = QFileDialog.getOpenFileName()[0]
 
-    def forward(self):
-        print("forward")
-
-    def back(self):
-        print("back")
+        if not g_path_to_csv:
+            print("файл не был выбран")
+        else:
+            db.Transfer(g_path_to_csv)
+            self.workerThread = db.WorkerThread()
+            self.workerThread.start()
 
 
 def main():
